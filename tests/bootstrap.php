defc4b9ae52dfa9a3ee87e470d300f881022a2d9
Bjoern Schiessle <bjoern@schiessle.org>
2017-12-15 17:22:57 +0100

    add travis
    Signed-off-by: Bjoern Schiessle <bjoern@schiessle.org>

---
+++ tests/bootstrap.php
@@ -0,0 +1,10 @@
+<?php
+if (!defined('PHPUNIT_RUN')) {
+	define('PHPUNIT_RUN', 1);
+}
+require_once __DIR__ . '/../../../lib/base.php';
+if(!class_exists('PHPUnit_Framework_TestCase')) {
+	require_once('PHPUnit/Autoload.php');
+}
+\OC_App::loadApp('end_to_end_encryption');
+OC_Hook::clear();
