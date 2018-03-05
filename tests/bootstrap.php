34837bb9f2c5c73a2fcccbdcab13661dab4482c8
Bjoern Schiessle <bjoern@schiessle.org>
2018-03-05 12:31:53 +0100

    fix travis
    Signed-off-by: Bjoern Schiessle <bjoern@schiessle.org>

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,7 +3,7 @@ if (!defined('PHPUNIT_RUN')) {
 	define('PHPUNIT_RUN', 1);
 }
 require_once __DIR__ . '/../../../lib/base.php';
-if(!class_exists('PHPUnit_Framework_TestCase')) {
+if(!class_exists('\PHPUnit\Framework\TestCase')) {
 	require_once('PHPUnit/Autoload.php');
 }
 \OC_App::loadApp('end_to_end_encryption');
