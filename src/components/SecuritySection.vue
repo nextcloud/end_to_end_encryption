97012a21e67d918dfcb2a5e6400134c82a3079c3
Carl Schwan <carl@carlschwan.eu>
2022-07-26 08:12:45 +0200

    Allow deleting encrypted files
    Signed-off-by: Carl Schwan <carl@carlschwan.eu>

--- src/components/SecuritySection.vue
+++ src/components/SecuritySection.vue
@@ -4,7 +4,8 @@
   -->
 
 <template>
-	<SettingsSection :title="t('end_to_end_encryption', 'End to End Encryption')">
+	<SettingsSection :title="t('end_to_end_encryption', 'End to End Encryption')"
+		:description="encryptionState">
 		<Button :disabled="!hasKey" type="warning" @click="startResetProcess()">
 			{{ t('end_to_end_encryption', 'Reset End to End Encryption') }}
 		</Button>
@@ -18,21 +19,33 @@
 				<li>{{ t('end_to_end_encryption', 'After deleting the keys any still existing device might cause problems, therefore please re-setup the accounts.') }}</li>
 			</ul>
 
+			<CheckboxRadioSwitch :checked.sync="deleteEncryptedFiles" type="switch">
+				{{ t('end_to_end_encryption', 'Delete existing encrypted files') }}
+			</CheckboxRadioSwitch>
+
 			<p class="margin-bottom">
 				{{ t('end_to_end_encryption', 'This is the final warning: Do you really want to reset your keys?') }}
 			</p>
-			<Button type="error"
-				@click="showModal()">
+			<Button type="error" @click="showModal">
 				{{ t('end_to_end_encryption', "Confirm") }}
 			</Button>
 
-			<modal v-if="modal" @close="closeModal" size="small">
-				<p>{{ t('end_to_end_encryption', 'Are you really sure?') }}
-				<Button type="error"
-					@click="resetEncryption()">
-					{{ t('end_to_end_encryption', "Confirm") }}
-				</Button>
-			</modal>
+			<Modal v-if="modal"
+				size="small"
+				class="modal"
+				@close="closeModal">
+				<div class="modal-container">
+					<p>{{ t('end_to_end_encryption', 'Are you really sure?') }}</p>
+					<div class="button-row">
+						<Button type="tertiary" @click="closeModal">
+							{{ t('end_to_end_encryption', "Cancel") }}
+						</Button>
+						<Button type="error" @click="resetEncryption">
+							{{ t('end_to_end_encryption', "Confirm") }}
+						</Button>
+					</div>
+				</div>
+			</Modal>
 		</div>
 	</SettingsSection>
 </template>
@@ -40,14 +53,15 @@
 <script>
 import axios from '@nextcloud/axios'
 import SettingsSection from '@nextcloud/vue/dist/Components/SettingsSection'
+import CheckboxRadioSwitch from '@nextcloud/vue/dist/Components/CheckboxRadioSwitch'
 import Button from '@nextcloud/vue/dist/Components/Button'
 import Modal from '@nextcloud/vue/dist/Components/Modal'
 import { loadState } from '@nextcloud/initial-state'
-import { showError } from '@nextcloud/dialogs'
+import { showError, showSuccess } from '@nextcloud/dialogs'
 import { getLoggerBuilder } from '@nextcloud/logger'
 import { generateOcsUrl } from '@nextcloud/router'
 
- const logger = getLoggerBuilder()
+const logger = getLoggerBuilder()
 	.setApp('settings')
 	.detectUser()
 	.build()
@@ -58,13 +72,26 @@ export default {
 		SettingsSection,
 		Button,
 		Modal,
+		CheckboxRadioSwitch,
 	},
 	data() {
 		return {
 			hasKey: loadState('end_to_end_encryption', 'hasKey'),
 			shouldDisplayWarning: false,
 			modal: false,
+			deleteEncryptedFiles: false,
 		}
+	},
+	computed: {
+		encryptionState() {
+			if (this.hasKey) {
+				return t('end_to_end_encryption', 'End to end encryption is currently enabled and correctly setup')
+			} else {
+				return t('end_to_end_encryption', 'End to end encryption is currently disabled. You can set it up with the {productName} clients.', {
+					productName: OCA.Theming ? OCA.Theming.name : 'Nextcloud',
+				})
+			}
+		},
 	},
 	methods: {
 		startResetProcess() {
@@ -76,30 +103,61 @@ export default {
 		closeModal() {
 			this.modal = false
 		},
+		async deletePrivateKey() {
+			const { data } = await axios.delete(generateOcsUrl('/apps/end_to_end_encryption/api/v1/private-key'))
+
+			return this.handleResponse({
+				status: data.ocs?.meta?.status,
+				error: null,
+			})
+		},
+		async deletePublicKey() {
+			const { data } = await axios.delete(generateOcsUrl('/apps/end_to_end_encryption/api/v1/public-key'))
+
+			return this.handleResponse({
+				status: data.ocs?.meta?.status,
+				error: null,
+			})
+		},
+		async deleteFiles() {
+			if (this.deleteEncryptedFiles) {
+				const { data } = await axios.delete(generateOcsUrl('/apps/end_to_end_encryption/api/v1/encrypted-files'))
+
+				return this.handleResponse({
+					status: data.ocs?.meta?.status,
+					error: null,
+				})
+			}
+			return true
+		},
 		async resetEncryption() {
 			try {
-				let { data } = await axios.delete(generateOcsUrl('/apps/end_to_end_encryption/api/v1/private-key'))
+				let success = true
+				success = success && await this.deletePrivateKey()
+				success = success && await this.deletePublicKey()
+				success = success && await this.deletePrivateKey()
 
-				this.handleResponse({
-					status: data.ocs?.meta?.status,
-				})
-
-				data = await axios.delete(generateOcsUrl('/apps/end_to_end_encryption/api/v1/public-key')).data
-				this.handleResponse({
-					status: data.ocs?.meta?.status,
-				})
+				if (success) {
+					showSuccess(t('end_to_end_encryption', 'End to end keys reset'))
+				}
 			} catch (e) {
 				this.handleResponse({
 					errorMessage: t('end_to_end_encryption', 'Unable to reset end to end encryption'),
 					error: e,
 				})
+			} finally {
+				this.closeModal()
+				this.shouldDisplayWarning = false
+				this.hasKey = false
 			}
 		},
 		async handleResponse({ status, errorMessage, error }) {
 			if (status !== 'ok') {
 				showError(errorMessage)
 				logger.error(errorMessage, { error })
+				return false
 			}
+			return true
 		},
 	},
 }
@@ -128,5 +186,18 @@ li {
 }
 .margin-bottom {
 	margin-bottom: 0.75rem;
+}
+
+.modal-container {
+	padding: 16px;
+	button {
+		margin-top: 16px;
+		margin-left: 8px;
+	}
+}
+
+.button-row {
+	display: flex;
+	justify-content: end;
 }
 </style>
