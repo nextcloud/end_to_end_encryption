b198db1d0e76413e1dc6e36a7181eb4bcb9b622b
Carl Schwan <carl@carlschwan.eu>
2022-09-01 15:30:36 +0200

    Implement limit to group feature
    - Add middleware to limit api call
    - Hide settings when access is disabled
    - Add admin settings to configure who has access
    
    Signed-off-by: Carl Schwan <carl@carlschwan.eu>
    (cherry picked from commit 468eb635dee50ae743ec6e45cb33fac1fcbb836f)

---
+++ lib/Middleware/CanUseAppMiddleware.php
@@ -0,0 +1,83 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * @copyright Copyright (c) 2019 Joas Schilling <coding@schilljs.com>
+ *
+ * @license GNU AGPL version 3 or any later version
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+namespace OCA\EndToEndEncryption\Middleware;
+
+use OCA\EndToEndEncryption\Config;
+use OCA\EndToEndEncryption\Middleware\Exceptions\CanNotUseAppException;
+use OCP\AppFramework\Controller;
+use OCP\AppFramework\Http;
+use OCP\AppFramework\Http\Response;
+use OCP\AppFramework\Http\RedirectToDefaultAppResponse;
+use OCP\AppFramework\Middleware;
+use OCP\AppFramework\OCS\OCSException;
+use OCP\AppFramework\OCSController;
+use OCP\AppFramework\Utility\IControllerMethodReflector;
+use OCP\IUser;
+use OCP\IUserSession;
+
+class CanUseAppMiddleware extends Middleware {
+	private IUserSession $userSession;
+	private IControllerMethodReflector $reflector;
+	private Config $config;
+
+	public function __construct(IUserSession $userSession,
+								IControllerMethodReflector $reflector,
+								Config $config) {
+		$this->userSession = $userSession;
+		$this->reflector = $reflector;
+		$this->config = $config;
+	}
+
+	/**
+	 * @param Controller $controller
+	 * @param string $methodName
+	 *
+	 * @throws CanNotUseAppException
+	 */
+	public function beforeController($controller, $methodName): void {
+		$user = $this->userSession->getUser();
+		if ($user instanceof IUser && $this->config->isDisabledForUser($user)) {
+			throw new CanNotUseAppException();
+		}
+	}
+
+	/**
+	 * @param Controller $controller
+	 * @param string $methodName
+	 * @param \Exception $exception
+	 * @throws \Exception
+	 * @return Response
+	 */
+	public function afterException($controller, $methodName, \Exception $exception): Response {
+		if ($exception instanceof CanNotUseAppException) {
+			if ($controller instanceof OCSController) {
+				throw new OCSException($exception->getMessage(), Http::STATUS_FORBIDDEN);
+			}
+
+			return new RedirectToDefaultAppResponse();
+		}
+
+		throw $exception;
+	}
+}
---
+++ lib/Settings/Admin.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+// SPDX-FileCopyrightText: 2022 Carl Schwan <carl@carlschwan.eu>
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+namespace OCA\EndToEndEncryption\Settings;
+
+use OCA\EndToEndEncryption\AppInfo\Application;
+use OCP\AppFramework\Http\TemplateResponse;
+use OCP\AppFramework\Services\IInitialState;
+use OCP\Settings\ISettings;
+use OCA\EndToEndEncryption\Config;
+
+class Admin implements ISettings {
+	private Config $config;
+	private IInitialState $initialState;
+
+	public function __construct(IInitialState $initialState, Config $config) {
+		$this->config = $config;
+		$this->initialState = $initialState;
+	}
+
+	public function getForm(): TemplateResponse {
+		$this->initialState->provideInitialState('allowed_groups', $this->config->getAllowedGroupIds());
+
+		return new TemplateResponse(
+			Application::APP_ID,
+			'settings-admin',
+			[]
+		);
+	}
+
+	public function getSection(): string {
+		return 'security';
+	}
+
+	public function getPriority(): int {
+		return 90;
+	}
+}
