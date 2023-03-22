ec55b08b369022fe4bff782df542d30341504abf
Louis Chemineau <louis@chmn.me>
2023-03-22 11:28:18 +0100

    [stable24] Bump API version to 1.2
    Signed-off-by: Louis Chemineau <louis@chmn.me>

--- lib/Capabilities.php
+++ lib/Capabilities.php
@@ -47,7 +47,7 @@ class Capabilities implements ICapability {
 		$capabilities = ['end-to-end-encryption' =>
 			[
 				'enabled' => true,
-				'api-version' => '1.1',
+				'api-version' => '1.2',
 			]
 		];
 
