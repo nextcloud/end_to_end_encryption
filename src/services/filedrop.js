f55c3fb3fc9535bd1104bf6f4dcec4d89655eedd
Louis Chemineau <louis@chmn.me>
2023-03-30 11:54:40 +0200

    Use AES to encrypt payload instead of RSA
    Signed-off-by: Louis Chemineau <louis@chmn.me>

--- src/services/filedrop.js
+++ src/services/filedrop.js
@@ -1,6 +1,6 @@
 import axios from '@nextcloud/axios'
 import { generateOcsUrl } from '@nextcloud/router'
-import { EncryptedFile, encryptStringAsymmetric } from './crypto.js'
+import { EncryptedFile, encryptStringAsymmetric, encryptWithAES, getRandomAESKey } from './crypto.js'
 
 /**
  * @typedef {object} EncryptedFileKey
@@ -15,7 +15,25 @@ import { EncryptedFile, encryptStringAsymmetric } from './crypto.js'
  * @property {string} encrypted - Encrypted JSON payload to the currently used metadata key. Encryption algorithm: AES/GCM/NoPadding (128 bit key size) with metadata key (symmetric)
  * @property {string} initializationVector - Initialization vector (ex: "+mHu52HyZq+pAAIN")
  * @property {string} authenticationTag - Authentication tag of the file (ex: "LYRaJghbZUzBiNWb51ypWw==")
+ * @property {string} encryptedKey - Encryption key to decrypt the 'encrypted' property
+ * @property {string} encryptedInitializationVector - Encryption initialization vector used to decrypt the 'encrypted' property.
+ * @property {string} encryptedTag - Encryption tag used to decrypt the 'encrypted' property.
  */
+
+async function getRandomEncryptionParams() {
+	return {
+		key: await getRandomAESKey(),
+		initializationVector: window.crypto.getRandomValues(new Uint8Array(16)),
+	}
+}
+
+/**
+ * @param {ArrayBuffer} buffer
+ * @return {string}
+ */
+function bufferToBase64(buffer) {
+	return btoa(String.fromCharCode(...new Uint8Array(buffer)))
+}
 
 /**
  * @param {EncryptedFile} file
@@ -28,22 +46,32 @@ export async function getFileDropEntry(file, tag, publicKey) {
 
 	/** @type {EncryptedFileKey} */
 	const encryptedPayload = {
-		key: btoa(String.fromCharCode(...new Uint8Array(rawFileEncryptionKey))),
+		key: bufferToBase64(rawFileEncryptionKey),
 		filename: file.originalFileName,
 		mimetype: file.mimetype,
 		version: '1.2',
 	}
 
-	const encrypted = await encryptStringAsymmetric(
+	const encryptedEncryptionParams = await getRandomEncryptionParams()
+
+	const encrypted = await encryptWithAES(
+		encryptedEncryptionParams,
+		new TextEncoder().encode(JSON.stringify(encryptedPayload))
+	)
+
+	const encryptedKey = await encryptStringAsymmetric(
 		publicKey,
-		new TextEncoder().encode(btoa(JSON.stringify(encryptedPayload)))
+		await window.crypto.subtle.exportKey('raw', encryptedEncryptionParams.key),
 	)
 
 	return {
 		[file.encryptedFileName]: {
-			encrypted: btoa(String.fromCharCode(...new Uint8Array(encrypted))),
-			initializationVector: btoa(String.fromCharCode(...file.initializationVector)),
-			authenticationTag: btoa(String.fromCharCode(...new Uint8Array(tag))),
+			encrypted: bufferToBase64(encrypted.content),
+			initializationVector: bufferToBase64(file.initializationVector),
+			authenticationTag: bufferToBase64(tag),
+			encryptedKey: bufferToBase64(encryptedKey),
+			encryptedTag: bufferToBase64(encrypted.tag),
+			encryptedInitializationVector: bufferToBase64(encryptedEncryptionParams.initializationVector),
 		},
 	}
 }
