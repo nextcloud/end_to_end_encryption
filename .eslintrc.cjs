59977e021867f248994ee3a80311dfd61c8ac39f
Louis Chemineau <louis@chmn.me>
2024-12-17 10:14:32 +0100

    chore: Switch to vite to build frontend
    And support typescript
    
    Signed-off-by: Louis Chemineau <louis@chmn.me>

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,7 +4,7 @@
  */
 module.exports = {
 	extends: [
-		'@nextcloud',
+		"@nextcloud/eslint-config/typescript"
 	],
 	globals: {
 		appName: true,
---
+++ vite.config.ts
@@ -0,0 +1,46 @@
+/**
+ * SPDX-FileCopyrightText: 2024 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+import type { UserConfig } from 'vitest/node'
+import { join } from 'path'
+
+import { createAppConfig } from '@nextcloud/vite-config'
+
+// replaced by vite
+declare const __dirname: string
+
+export default createAppConfig({
+	settings: join(__dirname, 'src', 'settings.js'),
+	adminSettings: join(__dirname, 'src', 'settings-admin.js'),
+	filedrop: join(__dirname, 'src', 'filedrop.js'),
+}, {
+	inlineCSS: { relativeCSSInjection: true },
+	extractLicenseInformation: true,
+	thirdPartyLicense: false,
+	config: {
+		// Setup for vitest unit tests
+		test: {
+			include: ['src/**/*.{test,spec}.?(c|m)[jt]s?(x)'],
+			environment: 'jsdom',
+			environmentOptions: {
+				jsdom: {
+					url: 'http://nextcloud.local',
+				},
+			},
+			coverage: {
+				include: ['src/**'],
+				exclude: ['**.spec.*', '**.test.*', '**.cy.*'],
+				provider: 'v8',
+				reporter: ['lcov', 'text'],
+			},
+			setupFiles: ['__tests__/setup-testing-library'],
+			server: {
+				deps: {
+					inline: [/@nextcloud\//],
+				},
+			},
+		} as UserConfig,
+	},
+})
