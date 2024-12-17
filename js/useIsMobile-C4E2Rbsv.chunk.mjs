443ece563805f5fe6f2b6b8d671ad284b5e2b8b3
nextcloud-command <nextcloud-command@users.noreply.github.com>
2024-12-17 16:42:21 +0000

    chore(assets): Recompile assets
    Signed-off-by: nextcloud-command <nextcloud-command@users.noreply.github.com>

---
+++ js/useIsMobile-C4E2Rbsv.chunk.mjs
@@ -0,0 +1,1 @@
+import{j as o,J as i}from"./_plugin-vue2_normalizer-CM8922Se.chunk.mjs";const n=1024,s=n/2,e=d=>document.documentElement.clientWidth<d,t=o(e(n)),r=o(e(s));window.addEventListener("resize",()=>{t.value=e(n),r.value=e(s)},{passive:!0});function c(){return i(t)}i(t);export{c as u};
