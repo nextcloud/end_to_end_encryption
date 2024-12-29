291077594c743e0e39c6b2ee70ecef6e7ab161df
nextcloud-command <nextcloud-command@users.noreply.github.com>
2024-12-29 03:12:30 +0000

    fix(deps): Fix npm audit
    Signed-off-by: GitHub <noreply@github.com>

--- js/logger-eZn2uTaV.chunk.mjs
+++
@@ -1,1 +0,0 @@
-import{b as e,a as t}from"./_plugin-vue2_normalizer-C21Me7LU.chunk.mjs";const o=n=>n===null?t().setApp("end_to_end_encryption").build():t().setApp("end_to_end_encryption").setUid(n.uid).build(),p=o(e());export{p as l};
