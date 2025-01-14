7872767a6b821a150f53aa1474ae772d105646dc
Louis Chemineau <louis@chmn.me>
2025-01-14 18:34:38 +0100

    chore: Compile assets
    Signed-off-by: Louis Chemineau <louis@chmn.me>

---
+++ js/logger-ROdD5nD9.chunk.mjs
@@ -0,0 +1,1 @@
+import{b as e,a as t}from"./_plugin-vue2_normalizer-CRvlGwWh.chunk.mjs";const o=n=>n===null?t().setApp("end_to_end_encryption").build():t().setApp("end_to_end_encryption").setUid(n.uid).build(),p=o(e());export{p as l};
