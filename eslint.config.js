8d40089021c203ac15117e280ddf45df37582683
Ferdinand Thiessen <opensource@fthiessen.de>
2025-10-15 12:45:34 +0200

    chore: migrate to ESLint v9
    Signed-off-by: Ferdinand Thiessen <opensource@fthiessen.de>

--- eslint.config.js
+++ eslint.config.js
@@ -2,11 +2,9 @@
  * SPDX-FileCopyrightText: 2022 Nextcloud GmbH and Nextcloud contributors
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
-module.exports = {
-	extends: [
-		"@nextcloud/eslint-config/typescript"
-	],
-	globals: {
-		appName: true,
-	},
-}
+
+import { recommendedVue2 } from '@nextcloud/eslint-config'
+
+export default [
+	...recommendedVue2,
+]
