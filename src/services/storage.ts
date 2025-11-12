0909a056ee316bc07ac8a04b1b74fc0231bf4695
Ferdinand Thiessen <opensource@fthiessen.de>
2025-11-12 14:48:50 +0100

    refactor: decompose browser warning component
    Signed-off-by: Ferdinand Thiessen <opensource@fthiessen.de>

---
+++ src/services/storage.ts
@@ -0,0 +1,17 @@
+/*!
+ * SPDX-FileCopyrightText: 2025 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+import { getBuilder } from '@nextcloud/browser-storage'
+
+/**
+ * Session storage - cleared when the browser/tab is closed.
+ */
+export const storage = getBuilder('end_to_end_encryption')
+	.persist(false)
+	.build()
+
+export const StorageKeys = Object.freeze({
+	SuppressBrowserWarning: 'suppress_browser_warning',
+})
