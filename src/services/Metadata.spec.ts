fb981266bb4ac8b64b343638bccf56ef661d4210
Ferdinand Thiessen <opensource@fthiessen.de>
2025-11-19 21:59:04 +0100

    test: add some unit tests for the Metadata class
    Signed-off-by: Ferdinand Thiessen <opensource@fthiessen.de>

---
+++ src/services/Metadata.spec.ts
@@ -0,0 +1,52 @@
+/*!
+ * SPDX-FileCopyrightText: 2025 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+import stringify from 'safe-stable-stringify'
+import { describe, expect, test } from 'vitest'
+import * as Alice from '../../__tests__/fixtures/Alice.spec.ts'
+import * as Bob from '../../__tests__/fixtures/Bob.spec.ts'
+import { base64ToBuffer, stringToBuffer } from './bufferUtils.ts'
+import { validateCMSSignature } from './crypto.ts'
+import { Metadata } from './Metadata.ts'
+
+describe('Creating metadata', () => {
+	test('should create new metadata instance', async () => {
+		const metadata = await Metadata.createNew()
+		expect(metadata).toBeInstanceOf(Metadata)
+		expect(metadata.getUsers()).toHaveLength(0)
+	})
+
+	test('should be able to add new user to metadata', async () => {
+		const metadata = await Metadata.createNew()
+		await metadata.addUser(Bob.userId, Bob.certificate)
+		expect(metadata.getUsers()).toEqual([Bob.userId])
+	})
+})
+
+describe('Export metadata', () => {
+	test('Export new metadata', async () => {
+		const cert = Alice.certificate
+		cert.privateKey = await getPrivateKey(Alice.privateKey)
+		const metadata = await Metadata.createNew()
+		await metadata.addUser(Alice.userId, cert)
+
+		const exported = await metadata.export(cert)
+		expect(exported.metadata.users).toHaveLength(1)
+		expect(exported.metadata.users[0]!.userId).toBe(Alice.userId)
+		expect(exported.metadata.filedrop).toBe(undefined)
+		expect(exported.metadata.version).toBe('2.0')
+
+		// validate signature
+		expect(await validateCMSSignature(
+			stringToBuffer(btoa(stringify(exported.metadata))),
+			base64ToBuffer(exported.signature),
+			exported.metadata.users,
+		)).toBe(true)
+	})
+})
+
+export async function getPrivateKey(privateKeyJwk: JsonWebKey) {
+	return await self.crypto.subtle.importKey('jwk', privateKeyJwk, { name: 'RSA-OAEP', hash: 'SHA-256' }, true, ['decrypt'])
+}
