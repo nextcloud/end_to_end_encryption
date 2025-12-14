08cac211fc0d65e99614ff132e1b197bf5bcea9a
nextcloud-command <nextcloud-command@users.noreply.github.com>
2025-12-14 03:27:08 +0000

    fix(deps): Fix npm audit
    Signed-off-by: GitHub <noreply@github.com>

---
+++ js/logger-vxI-tjXt.chunk.mjs
@@ -0,0 +1,2 @@
+import{b as e,a as t}from"./_plugin-vue2_normalizer-LjRcT802.chunk.mjs";const o=n=>n===null?t().setApp("end_to_end_encryption").build():t().setApp("end_to_end_encryption").setUid(n.uid).build(),p=o(e());export{p as l};
+//# sourceMappingURL=logger-vxI-tjXt.chunk.mjs.map
---
+++ js/useModelMigration-EhAWvqDD-D0Dhb2rW.chunk.mjs
@@ -0,0 +1,2 @@
+import{ao as d,V as i,z as s}from"./_plugin-vue2_normalizer-LjRcT802.chunk.mjs";function u(o,r,t=!1){const e=d().proxy;return t&&e.$props[o]===void 0&&e.$props.modelValue===void 0&&i.util.warn('Missing required prop: "modelValue" or old "'.concat(o,'"')),s({get(){return e.$props[o]!==void 0?e.$props[o]:e.$props.modelValue},set(p){e.$emit("update:modelValue",p),e.$emit("update:model-value",p),e.$emit(r,p)}})}export{u};
+//# sourceMappingURL=useModelMigration-EhAWvqDD-D0Dhb2rW.chunk.mjs.map
