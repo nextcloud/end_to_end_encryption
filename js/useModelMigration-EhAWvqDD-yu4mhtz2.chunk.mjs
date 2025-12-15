b4cf87f387ce22089c78cc111a4fdb79e48c3c0f
Ferdinand Thiessen <opensource@fthiessen.de>
2025-12-15 11:36:01 +0100

    chore: compile assets
    Signed-off-by: Ferdinand Thiessen <opensource@fthiessen.de>

---
+++ js/useModelMigration-EhAWvqDD-yu4mhtz2.chunk.mjs
@@ -0,0 +1,2 @@
+import{al as d,V as i,x as s}from"./_plugin-vue2_normalizer-CBaDPvNb.chunk.mjs";function n(o,r,t=!1){const e=d().proxy;return t&&e.$props[o]===void 0&&e.$props.modelValue===void 0&&i.util.warn('Missing required prop: "modelValue" or old "'.concat(o,'"')),s({get(){return e.$props[o]!==void 0?e.$props[o]:e.$props.modelValue},set(p){e.$emit("update:modelValue",p),e.$emit("update:model-value",p),e.$emit(r,p)}})}export{n as u};
+//# sourceMappingURL=useModelMigration-EhAWvqDD-yu4mhtz2.chunk.mjs.map
