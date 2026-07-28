3ec6767f014a4938c3baa3e7865f51ba3349f28f
Ferdinand Thiessen <opensource@fthiessen.de>
2026-07-28 14:49:37 +0200

    chore: add reflection metadata polyfill
    Signed-off-by: Ferdinand Thiessen <opensource@fthiessen.de>

--- src/settings-admin.ts
+++ src/settings-admin.ts
@@ -6,5 +6,7 @@
 import { createApp } from 'vue'
 import SettingsAdmin from './views/SettingsAdmin.vue'
 
+import 'core-js/proposals/reflect-metadata.js' // for @peculiar/x509
+
 const app = createApp(SettingsAdmin)
 app.mount('#security-admin-end-to-end')
