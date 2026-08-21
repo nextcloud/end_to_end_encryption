b032cfac0c7004c8e01d2d9b2b1df0d631a0adf4
Josh <josh.t.richards@gmail.com>
2026-08-21 13:11:06 -0400

    test: adjust MetaDataStorageTest test count expect
    Signed-off-by: Josh <josh.t.richards@gmail.com>

--- tests/Unit/MetaDataStorageTest.php
+++ tests/Unit/MetaDataStorageTest.php
@@ -612,7 +612,7 @@ class MetaDataStorageTest extends TestCase {
 				->with('/meta-data/42')
 				->willReturn($metaDataFolder);
 
-			$metaDataFolder->expects($this->exactly(2))
+			$metaDataFolder->expects($this->exactly(3))
 				->method('fileExists')
 				->willReturnCallback(fn (string $name): bool => match ($name) {
 					'intermediate.meta.data',
@@ -633,7 +633,7 @@ class MetaDataStorageTest extends TestCase {
 				$intermediateSignatureFile->expects($this->once())
 					->method('delete');
 
-				$metaDataFolder->expects($this->exactly(2))
+				$metaDataFolder->expects($this->exactly(3))
 					->method('getFile')
 					->willReturnCallback(fn (string $name): ISimpleFile => match ($name) {
 						'intermediate.meta.data' => $intermediateFile,
