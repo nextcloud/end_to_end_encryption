0bbe7a4f7379c0be203f3cae39a5c38b5ab3099a
nextcloud-command <nextcloud-command@users.noreply.github.com>
2026-08-30 08:57:10 +0000

    fix(deps): Fix npm audit
    Signed-off-by: GitHub <noreply@github.com>

---
+++ js/useIsMobile-C2VytaIH.chunk.mjs
@@ -0,0 +1,2 @@
+import{w as i,Z as n}from"./_plugin-vue2_normalizer-IK_K5uWo.chunk.mjs";const t=1024,s=t/2,e=r=>document.documentElement.clientWidth<r,o=i(e(t)),a=i(e(s));window.addEventListener("resize",()=>{o.value=e(t),a.value=e(s)},{passive:!0});function c(){return n(o)}function d(){return n(a)}n(o);export{d as a,c as u};
+//# sourceMappingURL=useIsMobile-C2VytaIH.chunk.mjs.map
