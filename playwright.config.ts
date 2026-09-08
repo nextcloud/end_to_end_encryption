b0dfacef3da6388a9b4682e39853e06fdf38e9c2
Ferdinand Thiessen <opensource@fthiessen.de>
2026-09-08 23:37:29 +0200

    ci: reduce CI overhead for playwright tests
    Signed-off-by: Ferdinand Thiessen <opensource@fthiessen.de>

--- playwright.config.ts
+++ playwright.config.ts
@@ -46,24 +46,16 @@ export default defineConfig({
 
 	projects: [
 		{
-			name: 'default',
+			name: 'chrome',
 			use: {
 				...BROWSWER_CONFIG_CHROME,
 			},
 		},
 
-		...(process.env.CI
-			? [
-					{
-						name: 'firefox',
-						use: { ...devices['Desktop Firefox'] },
-					},
-					// {
-					//   name: 'webkit',
-					//   use: { ...devices['Desktop Safari'] },
-					// },
-				]
-			: []),
+		{
+			name: 'firefox',
+			use: { ...devices['Desktop Firefox'] },
+		},
 	],
 
 	webServer: {
