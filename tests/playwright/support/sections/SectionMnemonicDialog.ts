7b31e1ce61107881cffcafdbfcd9f6e18eff6dfb
Ferdinand Thiessen <opensource@fthiessen.de>
2026-09-08 23:56:46 +0200

    test: resolve test issues
    Signed-off-by: Ferdinand Thiessen <opensource@fthiessen.de>

--- tests/playwright/support/sections/SectionMnemonicDialog.ts
+++ tests/playwright/support/sections/SectionMnemonicDialog.ts
@@ -9,19 +9,20 @@ import { expect } from '@playwright/test'
 
 export class SectionMnemonicDialog {
 	public readonly buttonSubmit: Locator
-	public readonly checkboxConsent: Locator
+	public readonly switchConsent: Locator
 	public readonly inputMnemonic: Locator
 
 	constructor(public readonly dialogLocator: Locator) {
 		this.buttonSubmit = this.dialogLocator.getByRole('button', { name: /Submit/i })
-		this.checkboxConsent = this.dialogLocator.getByRole('checkbox', { name: /I understand the risks/i })
+		// NcCheckboxRadioSwitch exposes `type="switch"` as role `switch`, not `checkbox`
+		this.switchConsent = this.dialogLocator.getByRole('switch', { name: /I understand the risks/i })
 		this.inputMnemonic = this.dialogLocator.getByRole('textbox', { name: /Mnemonic/i })
 	}
 
 	public async fillAndSubmit(mnemonic: string): Promise<void> {
 		await expect(this.dialogLocator).toBeVisible()
 		await this.inputMnemonic.fill(mnemonic)
-		await this.checkboxConsent.check({ force: true })
+		await this.switchConsent.check({ force: true })
 		await expect(this.buttonSubmit).not.toBeDisabled()
 		await this.buttonSubmit.click()
 		await expect(this.dialogLocator).toHaveCount(0)
