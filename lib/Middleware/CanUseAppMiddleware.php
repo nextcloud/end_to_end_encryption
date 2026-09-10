824fd765149996e1843d39e4c4a3eb8dcb257278
Josh <josh.t.richards@gmail.com>
2026-09-09 22:03:38 -0400

    chore: fixup CanUseAppMiddleware.php
    Signed-off-by: Josh <josh.t.richards@gmail.com>

--- lib/Middleware/CanUseAppMiddleware.php
+++ lib/Middleware/CanUseAppMiddleware.php
@@ -12,7 +12,7 @@ use OCA\EndToEndEncryption\Config;
 use OCA\EndToEndEncryption\Middleware\Exceptions\CanNotUseAppException;
 use OCP\AppFramework\Controller;
 use OCP\AppFramework\Http;
-use OCP\AppFramework\Http\RedirectResponse
+use OCP\AppFramework\Http\RedirectResponse;
 use OCP\AppFramework\Http\Response;
 use OCP\AppFramework\Middleware;
 use OCP\AppFramework\OCS\OCSException;
